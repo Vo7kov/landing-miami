4ef2138035855096558ae3caa30e5a0d0c18d8d6
Vo7kov <drainys1234@gmail.com>
2023-03-26 14:32:46 +0300

    miami landing was added

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
